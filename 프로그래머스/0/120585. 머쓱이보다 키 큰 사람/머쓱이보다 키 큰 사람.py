c22b910213d5f8f34b05d29ae94a2cf0e6462f26
Sia <102781433+siaSim@users.noreply.github.com>
2026-06-16 15:18:25 +0900

    Refactor code by cleaning up comments and formatting
    Removed commented-out code and added newlines for clarity.

--- 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.py
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.py
@@ -6,3 +6,10 @@ def solution(array, height):
             answer += 1
     
     return answer
+
+
+
+# def solution(array, height):
+#     array.append(height)
+#     array.sort(reverse=True)
+#     return array.index(height)
