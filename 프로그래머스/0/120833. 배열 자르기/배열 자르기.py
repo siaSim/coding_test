90a94a9f83b4d422c110f05a3c259803ff025b58
Sia <102781433+siaSim@users.noreply.github.com>
2026-06-16 16:11:05 +0900

    [level 0] Title: 배열 자르기, Time: 0.00 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/0/120833. 배열 자르기/배열 자르기.py
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.py
@@ -1,7 +1,2 @@
 def solution(numbers, num1, num2):
-    answer = []
-    
-    for i in range(num1, num2+1):
-        answer.append(numbers[i])
-    
-    return answer
+    return numbers[num1:num2+1]
