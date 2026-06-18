416e6ed2dee21e9bfc5a3d04e5219aa4a1f1c6db
Sia <102781433+siaSim@users.noreply.github.com>
2026-06-18 17:50:43 +0900

    Fix return statement and clean up code

--- 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.py
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.py
@@ -5,3 +5,7 @@ def solution(my_string, n):
         answer += i*n
         # answer = answer + (i*n)
     return answer
+
+
+# def solution(my_string, n):
+#     return ''.join(i * n for i in my_string)
