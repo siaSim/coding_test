9467260d5cc5eeb5757ebf259b733ef05308697a
Sia <102781433+siaSim@users.noreply.github.com>
2026-06-22 12:23:54 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.01 ms, Memory: 11.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
@@ -0,0 +1,9 @@
+def solution(my_string):
+    answer = []
+     
+    for str in my_string:
+        if str in "0123456789":
+            answer.append(int(str))
+    
+    answer.sort()
+    return answer
